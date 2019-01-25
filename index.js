32fdd2e0bb2c5984358a53db883d85d802384966
harpalgupta <harpal.gupta@gmail.com>
2019-01-25 08:36:36 +0000

    add check for timeInBusiness

--- index.js
+++ index.js
@@ -4,11 +4,18 @@ const input=require('./inputs/1.json');
 const elegibilityCheck=(input)=>{
 //loan request amount between 5000 and 50000
 if (input.amountRequested>=5000&&input.amountRequested<=50000) {console.log('requested valid amount')} 
-else return false  
-}
+else return false; 
+
 
 //* The business must have been operating for more than 12 months
-if (input.timeInBusiness.years>=1)
+if (input.timeInBusiness.years>=1){
+console.log('time in business is at least 1 year')
+}
+else return false;
+
+return true;
+}
+
 
 
 

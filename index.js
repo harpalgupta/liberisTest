5ef680012cac1d8f409c159f63a670dc1c37366f
harpalgupta <harpal.gupta@gmail.com>
2019-01-25 08:31:48 +0000

    add check for valid amount requested

---
+++ index.js
@@ -0,0 +1,18 @@
+//take in example input
+const input=require('./inputs/1.json');
+
+const elegibilityCheck=(input)=>{
+//loan request amount between 5000 and 50000
+if (input.amountRequested>=5000&&input.amountRequested<=50000) {console.log('requested valid amount')} 
+else return false  
+}
+
+//* The business must have been operating for more than 12 months
+if (input.timeInBusiness.years>=1)
+
+
+
+console.log(elegibilityCheck(input))
+
+
+module.exports= elegibilityCheck;
